6f8b6800ae935483642efdc3066d9e05074389a1
unknown <dzirimohamedayman@gmail.com>
2025-12-03 14:33:21 +0000

    creating eye service

---
+++ src/main/java/com/opticpro/opticpro/repository/EyeRepository.java
@@ -0,0 +1,7 @@
+package com.opticpro.opticpro.repository;
+
+import com.opticpro.opticpro.entity.eye.Eye;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EyeRepository extends JpaRepository<Eye, Integer> {
+}
